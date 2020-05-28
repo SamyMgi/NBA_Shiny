eaf1d4efd3f698c3915564caf92a75e33baf9587
SamyMgi <65293621+SamyMgi@users.noreply.github.com>
2020-05-28 12:03:34 +0200

    Amélioration de la première page : radarplot
    Ajout du radarplot pour comparer les joueurs 2 à 2

--- app.R
+++ app.R
@@ -9,28 +9,51 @@ library(forcats)
 library(dplyr)
 library(dbConnect)
 library(plotly)
+library(fmsb)
+library(questionr)
 
 #nba <- read.csv2("nbaNew.csv", sep=",")
 
 #Importation données
 smp = read_csv(file = "nbaNew.csv")
 
-#Enlever les 5 dernieres lignes
+#Enlever les 5 dernieres lignes : contenants beaucoup de virgules mais sans données
 nbligne=nrow(smp)
 nblignebis=nbligne-5
 nba=smp[-c(nblignebis:nbligne),]
 
+#Palette de couleurs pour le radar plot
+colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9) )
+colors_in=c( rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.4) , rgb(0.7,0.5,0.1,0.4) )
+
+
 #Création des colonnes moyennes (Points, Rebonds, Passes décisives)
 nba <- mutate(nba, moypts = round(nba$PTS / nba$G, 1), moyreb = round(nba$TRB / nba$G, 1), moypad = round(nba$AST / nba$G, 1))
+
 #Et classement en fonction de l'année
 nba <- nba %>% arrange(desc(SeasonStart))
+nba
 
-#Recodage de la variable année en facteur
+#Renommer variables
+nba <- rename.variable(nba, "FG%", "XFG")
+nba <- rename.variable(nba, "eFG%", "XeFG")
+nba <- rename.variable(nba, "3P%", "X3P")
+nba <- rename.variable(nba, "2P%", "X2P")
+nba <- rename.variable(nba, "FT%", "XFT")
 
+#Recodage des variables : caractère -> numérique
+nba$XFG<-as.numeric(gsub("%","",as.character(nba$XFG)))
+nba$XeFG<-as.numeric(gsub("%","",as.character(nba$XeFG)))
+nba$X3P<-as.numeric(gsub("%","",as.character(nba$X3P)))
+nba$X2P<-as.numeric(gsub("%","",as.character(nba$X2P)))
+nba$XFT<-as.numeric(gsub("%","",as.character(nba$XFT)))
+nba$X3P=100*nba$X3P
+nba$SeasonStart=as.character(nba$SeasonStart)
 
 
 
 ui = dashboardPage(
+  
   dashboardHeader(
     title = "NBA",
     titleWidth =  300
@@ -45,8 +68,9 @@ ui = dashboardPage(
     )
   ),
   dashboardBody(
-    tabItems(         #Forme du première onglet
+    tabItems(         #Forme du premier onglet
       tabItem("meilleurs",
+              fluidRow(
               box(
                 width = 3,
                 selectInput("saison",
@@ -87,7 +111,39 @@ ui = dashboardPage(
                      ),
                      img(src='russwest.gif',style="display: block; margin-left: auto; margin-right: auto; width: 100%;",height = 250, width = 350)
                      
+              )),
+              fluidRow(
+                infoBox( 
+                  title =" ",
+                  width = 13,
+                  value = tags$p("Qui est le meilleur tireur ? A vous de comparer !", style = "font-size: 150%;"), 
+                  icon=icon("percent"),
+                  color = "blue"
+                ),
+              box(
+                width = 4,
+                selectInput("joueur1",
+                            "Choisissez un premier joueur",
+                            choices = c(
+                              "Tous les joueurs",
+                              unique(nba$PlayerName)
+                            ))
+              ),
+              box(
+                title = "Comparaison des pourcentages de réussite",
+                plotOutput("spiderweb"),
+                width = 4
+              ),
+              box(
+                width = 4,
+                selectInput("joueur2",
+                            "Choisissez un second joueur",
+                            choices = c(
+                              "Tous les joueurs",
+                              unique(nba$PlayerName)
+                            )))
               )
+              
               
               
               
@@ -127,7 +183,8 @@ ui = dashboardPage(
 )
 
 server <- function(input, output) {
-  donnees = reactive({
+  #Choix de la saison
+  choix_saison = reactive({
     if (input$saison == "Toutes les saisons") {
       onlyseason = nba
     } else {
@@ -135,10 +192,28 @@ server <- function(input, output) {
     }
     onlyseason
   })
+  #1er choix de joueur pour le radar plot
+  choix_joueur1 = reactive({
+    if (input$joueur1 == "Tous les joueurs") {
+      onlyplayers = nba %>% filter(nba$PlayerName=="LeBron James")
+    } else {
+      onlyplayers = nba %>% filter(nba$PlayerName == input$joueur1)
+    }
+    onlyplayers
+  })
+  #2ème choix de joueur pour le radarplot
+  choix_joueur2 = reactive({
+    if (input$joueur2 == "Tous les joueurs") {
+      onlyplayers2 = nba %>% filter(nba$PlayerName=="Kobe Bryant")
+    } else {
+      onlyplayers2 = nba %>% filter(nba$PlayerName == input$joueur2)
+    }
+    onlyplayers2
+  })
   
   #Affichage des 10 meilleurs scoreurs par saison
   output$classement_points = renderTable({
-    only1=donnees()
+    only1=choix_saison()
     nba %>% 
       filter(SeasonStart==only1$SeasonStart) %>% 
       select(PlayerName, moypts) %>% 
@@ -150,7 +225,7 @@ server <- function(input, output) {
   
   #Affichage des 10 meilleurs passeurs par saison
   output$classement_pad = renderTable({
-    only1=donnees()
+    only1=choix_saison()
     nba %>% 
       filter(SeasonStart==only1$SeasonStart) %>% 
       select(PlayerName, moypad) %>% 
@@ -162,7 +237,7 @@ server <- function(input, output) {
   
   #Affichage des 10 meilleurs rebondeurs par saison
   output$classement_reb = renderTable({
-    only1=donnees()
+    only1=choix_saison()
     nba %>% 
       filter(SeasonStart==only1$SeasonStart) %>% 
       select(PlayerName, moyreb) %>% 
@@ -189,14 +264,8 @@ server <- function(input, output) {
     plot(NBA_joueur$SeasonStart, NBA_joueur$moypts, main="Moyenne des points par match", xlab="Saison", ylab="Moy points")
   })
   
-#  output$player_points = renderText({
-  #    joueur_choisi1=joueur_choisi()
-  #    meilleur_moyenne = nba %>% filter(PlayerName %in% joueur_choisi1$PlayerName) %>% arrange(desc(moypts)) %>% slice(1:2)
-  #    paste(meilleur_moyenne$moypts, "est la meilleur moyenne de points par match de", meilleur_moyenne$PlayerName)
-    
-  #  })
+  #Représentation des triple double de moyenne
   output$triple_double = renderTable({ 
-    only1=donnees()
     nba %>% 
       filter(moypts>=10 & moyreb>=10 & moypad>=10) %>% 
       select(SeasonStart, PlayerName, moypts, moypad, moyreb) %>% 
@@ -207,12 +276,33 @@ server <- function(input, output) {
              "Passes decisives par match" = moypad,
              "Rebonds par match" = moyreb)
   })
+  #Case anecdote sur le Triple Double
   output$texte_td = renderText({ 
-    paste("55 ans après Roberson, Westbrook devient le deuxième joueur de 
+    paste("55 ans après Robertson, Westbrook devient le deuxième joueur de 
           l'histoire a réaliser un triple double de moyenne sur une saison.
           Performance qu'il reproduira l'année suivante, faisant de lui le seul joueur de l'histoire 
           à réaliser un
-          'back-to-back triple double'")
+          'back-to-back triple double'.")
+  })
+  #Radar plot 
+  output$spiderweb = renderPlot({
+        only_player1=choix_joueur1()
+        only_player2=choix_joueur2()
+        newbdd=nba %>% 
+            filter(PlayerName==only_player1$PlayerName | PlayerName==only_player2$PlayerName) %>% 
+            select(PlayerName, XFG, XeFG, X3P, X2P, XFT) 
+        newbdd=aggregate(newbdd,list(newbdd$PlayerName), mean)
+        newbdd=newbdd[,-c(1:2)]
+        newbdd <- rbind(rep(100,5) , rep(0,5) , newbdd)
+        radarchart( newbdd  , axistype=1 , 
+                #custom polygon
+                pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
+                #custom the grid
+                cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,100,25), cglwd=0.8,
+                #custom labels
+                vlcex=0.8 
+    )
+    
   })
   
 }
